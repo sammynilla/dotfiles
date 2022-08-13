3a3177e390cf632dde79d6de960c2c94f44900ad
Samantha Bartlett <sammynilla@protonmail.com>
2022-08-13 01:04:12 -0400

    removed some includes that weren't getting used

--- linux/.local/st/x.c
+++ linux/.local/st/x.c
@@ -1,17 +1,12 @@
 /* See LICENSE for license details. */
 #include <errno.h>
 #include <math.h>
-#include <limits.h>
 #include <locale.h>
-#include <signal.h>
 #include <sys/select.h>
 #include <time.h>
 #include <unistd.h>
-#include <libgen.h>
 #include <X11/Xatom.h>
-#include <X11/Xlib.h>
 #include <X11/cursorfont.h>
-#include <X11/keysym.h>
 #include <X11/Xft/Xft.h>
 #include <X11/XKBlib.h>
 
