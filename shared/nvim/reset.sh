e0ec944c8590055dfb25063efd2732d72f6a83b0
Samantha Bartlett <sammynilla@protonmail.com>
2023-02-12 19:40:31 -0900

    lsp_server folder is for the old lsp installer. we've moved to mason

--- shared/nvim/reset.sh
+++ shared/nvim/reset.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-# :UpdateRemotePlugin output
-rm -f "$HOME/.local/share/nvim/rplugin.vim"
-# installed lsp's
-rm -rf "$HOME/.local/share/nvim/lsp_servers"
+# installed lsp's (mason installer)
+rm -rf "$HOME/.local/share/nvim/mason"
 # packer plugins
 rm -f "$HOME/.local/share/nvim/site/plugin/packer_compiled.lua"
 rm -rf "$HOME/.local/share/nvim/site/pack/packer"
