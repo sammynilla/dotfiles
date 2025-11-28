99fe3c94344e3887df035a95f240bb655eada835
Samantha Bartlett <sammynilla@protonmail.com>
2025-11-28 12:03:21 -0900

    deciding to ftdetect via modeline

--- .config/nvim/ftdetect/gbz80.lua
+++ .config/nvim/ftdetect/gbz80.lua
@@ -1,7 +1,6 @@
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufFilePre", "BufRead" }, {
-	pattern = { "*.gbasm", "*.gbz80", "*.inc" },
+	pattern = { "*.asm", "*.inc" },
 	callback = function()
-		vim.cmd.set("filetype=gbz80")
 		vim.cmd.set("commentstring=;\\ %s")
 	end,
 })
